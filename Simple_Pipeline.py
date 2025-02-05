8bd40e69e564cab82caf697d1b28a56ec75c548f
Settara Pramod <57582343+settarapramod@users.noreply.github.com>
2025-02-05 10:29:55 +0530

    Update Simple_Pipeline.py

--- Simple_Pipeline.py
+++ Simple_Pipeline.py
@@ -1,10 +1,11 @@
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions, StreamingOptions
+from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io.kafka import ReadFromKafka
 
 # Kafka configurations
-BOOTSTRAP_SERVERS = 'localhost:9092'  # Update with your Kafka broker address
-TOPIC = 'your_topic'  # Update with your Kafka topic
+BOOTSTRAP_SERVERS = 'localhost:9092'  # Update this
+TOPIC = 'your_topic'  # Update this
+GROUP_ID = 'beam_consumer_group'  # Change to any unique group ID
 
 class PrintMessages(beam.DoFn):
     def process(self, element):
@@ -14,10 +15,18 @@ class PrintMessages(beam.DoFn):
 
 def run():
     pipeline_options = PipelineOptions(streaming=True)  # Enable streaming mode
+    
     with beam.Pipeline(options=pipeline_options) as p:
         (
             p 
-            | 'ReadFromKafka' >> ReadFromKafka(consumer_config={'bootstrap.servers': BOOTSTRAP_SERVERS}, topics=[TOPIC])
+            | 'ReadFromKafka' >> ReadFromKafka(
+                consumer_config={
+                    'bootstrap.servers': BOOTSTRAP_SERVERS,
+                    'group.id': GROUP_ID,  # Consumer group ID
+                    'auto.offset.reset': 'earliest'  # Start from the beginning
+                },
+                topics=[TOPIC]
+            )
             | 'PrintMessages' >> beam.ParDo(PrintMessages())
         )
 
